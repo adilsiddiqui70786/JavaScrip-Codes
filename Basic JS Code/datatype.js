e58ce75a321fbb5580c911ee3c0d7e4331d92190
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-08-17 18:54:36 +0530

    datatype.js

---
+++ Basic JS Code/datatype.js
@@ -0,0 +1,10 @@
+let numm = 101;
+
+console.log(typeof numm);
+// typeof keyworld is used to know the exact data type of the varaiable in the js :
+
+var onenum = 50;
+
+var twonum = onenum;
+
+console.log(twonum);
